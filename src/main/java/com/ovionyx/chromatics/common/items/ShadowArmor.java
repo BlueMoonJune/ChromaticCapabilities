7c55b829507d82ee0db99284ba08fce5345ca549
JerryLu086 <77890752+JerryLu086@users.noreply.github.com>
2021-09-21 14:57:19 +0800

    Some fixes of the armor
    - Made some changes that will no longer require a separated class of the chest piece to make the set work.

--- src/main/java/com/ovionyx/chromatics/common/items/ShadowArmor.java
+++ src/main/java/com/ovionyx/chromatics/common/items/ShadowArmor.java
@@ -1,15 +1,29 @@
 package com.ovionyx.chromatics.common.items;
 
 import com.simibubi.create.foundation.utility.VecHelper;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.LazyValue;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import com.ovionyx.chromatics.Chromatics;
 
+@Mod.EventBusSubscriber(modid = Chromatics.MODID)
 public class ShadowArmor extends ArmorItem {
     public ShadowArmor(IArmorMaterial p_i48534_1_, EquipmentSlotType p_i48534_2_, Properties p_i48534_3_) {
         super(p_i48534_1_, p_i48534_2_, p_i48534_3_);
@@ -32,5 +46,40 @@ public class ShadowArmor extends ArmorItem {
             world.addParticle(ParticleTypes.END_ROD, basemotion.x, positionVec.y, basemotion.z, 0.0D, -0.10000000149011612D, 0.0D);
         }
         return false;
+    }
+    
+    @SubscribeEvent
+    public static void onLivingTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            PlayerEntity player = event.player;
+            int pieces = 0;
+
+            if (player.getPosY() <= 0) {
+                Iterable<ItemStack> armorItems = player.getArmorInventoryList();
+
+                for (ItemStack stack : armorItems) {
+                    if (stack.getItem() instanceof ShadowArmor) {
+                        if (!player.world.isRemote && player.getCooldownTracker().hasCooldown(stack.getItem()))
+                            return;
+                        pieces++;
+                    }
+                }
+                if (pieces == 4) {
+                    Vector3d playerVec = player.getPositionVec();
+                    Vector3d particlePos = VecHelper.offsetRandomly(playerVec, player.world.getRandom(), 1.5F);
+                    player.world.addParticle(ParticleTypes.END_ROD, particlePos.getX(), particlePos.getY(), particlePos.getZ(), 0, -0.10000000149011612D, 0);
+
+                    if (!player.world.isRemote)
+                        player.addPotionEffect(new EffectInstance(Effects.LEVITATION, 5, 100));
+                        for (ItemStack stack : armorItems) {
+                            stack.damageItem((int)(5 + player.world.getRandom().nextFloat() * 5), player, (entity) -> {
+                                entity.sendBreakAnimation(stack.getEquipmentSlot());
+                            });
+                            player.getCooldownTracker().setCooldown(stack.getItem(), 60);
+                        }
+
+                }
+            }
+        }
     }
 }
